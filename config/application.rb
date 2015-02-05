5553dec23a6629a34540a23fb05459300afc5c08
deskinterview <deskinterview@users.noreply.github.com>
2015-02-05 03:05:12 -0500

    update to try to make Heroku happy. It does not like initing the DB for assets::precompile

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module Deskapi
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Added to try to get working on Heroku
+    config.assets.initialize_on_precompile = false
   end
 end
